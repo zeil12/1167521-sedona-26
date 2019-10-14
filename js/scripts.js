330d486f307f8167182ce3e8004e7a292b4d1080
zeil12 <54764677+zeil12@users.noreply.github.com>
2019-10-15 01:33:34 +0300

    Пиксель перфект и слайдер

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,3 @@
-$(document).ready(function() {
-    $("#cost-slider").ionRangeSlider({
-        type: 'double',
-        min: 0,
-        max: 3000,
-    });
-});
 document.querySelector('.modal-button').onclick = function () {
     document.querySelector('.modal-window').classList.toggle('closed');
 };
